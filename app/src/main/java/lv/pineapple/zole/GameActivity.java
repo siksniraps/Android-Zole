274384792e0c116d53a32bc6bbc2b4be30d0749a
siksniraps <davis.sparinskis@gmail.com>
2014-06-15 20:15:52 +0300

    Initial commit with a crude game ui and no game logic.

---
+++ app/src/main/java/lv/pineapple/zole/GameActivity.java
@@ -0,0 +1,15 @@
+package lv.pineapple.zole;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Game game = new Game();
+    }
+
+}
